4fc470bcdfc3ed522773d70d49b8affa22914873
Wexpo Lyu <laosb@lns.unihubs.com>
2016-06-23 13:06:06 +0800

    Update commands-packages.js

--- tools/cli/commands-packages.js
+++ tools/cli/commands-packages.js
@@ -1652,7 +1652,7 @@ main.registerCommand({
   var upgradePackagesWithoutCordova = _.reject(upgradePackageNames, function (name) {
     return name.split(':')[0] === 'cordova'; 
   });
-  if (upgradePackagesWithoutCordova !== upgradePackageNames) {
+  if (upgradePackagesWithoutCordova.sort().toString() !== upgradePackageNames.sort().toString()) {
     // There are some cordova packages in the list to update.
     // We should tell users how to update cordova packages.
     Console.warn('Looks like you\'re trying to update cordova packages.');
